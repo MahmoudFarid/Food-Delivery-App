4c34bbc261b683b3f6b7a4c273b9142ad66434b0
mahmoud farid <mahmoud.farid.94@gmail.com>
2018-12-23 23:42:15 +0200

    Remove unused files

--- food_delivery_app/restaurants/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Restaurant, Category, ItemSize, Item
-
-
-admin.site.register(Restaurant)
-admin.site.register(Category)
-admin.site.register(ItemSize)
-admin.site.register(Item)
